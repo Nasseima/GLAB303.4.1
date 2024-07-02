2eadc79820d3a9b7ee442590f3c3a3a18665878d
Nasseima <lindsaynasseima@gmail.com>
2024-07-02 11:51:23 -0400

    Continuing with my control flow assignment

---
+++ src/Question1.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Question1 {
+    public static void main(String[] args) {
+
+        int x = 7;
+        if(x < 10){
+            System.out.println("Less than 10");
+        }
+    }
+}
